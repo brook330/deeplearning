feecdf3ad31e2b3010da101a0ad44e9e684f57a7
newusually <49580033+newusually@users.noreply.github.com>
2021-05-30 15:26:03 +0800

    Add files via upload

--- deeplearning.py
+++ deeplearning.py
@@ -137,7 +137,7 @@ class Datainfo:
 
         dfclose = pd.read_csv(f'./datas/okex/eth/ethclose.csv').set_index('timestamps')
 
-        df2 = df2.merge(dfclose, left_index=True, right_index=True, how='left')
+        df2 = df2.merge(dfclose, left_on='timestamps',right_on='timestamps', how='left')
 
         df2.to_csv(f'./datas/okex/eth/ethusd_final.csv',index = True)
         Datainfo.saveinfo('保存所有的 ethusd 数据完毕。。。   ')
