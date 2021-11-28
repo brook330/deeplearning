460432fb8c68b9451e61090a7a5297850223c297
newusually <49580033+newusually@users.noreply.github.com>
2021-11-28 21:07:56 +0800

    Add files via upload

--- minidaoqiongsi.py
+++ minidaoqiongsi.py
@@ -21,78 +21,74 @@ class NQ_Datainfo():
    
     def getdatainfo_minute(self,minute,symbol):
 
-        for i in range(10000):
+        
 
-            try:
+        time.sleep(minute/100)
 
-                time.sleep(minute/100)
+        t = time.time()
 
-                t = time.time()
+        #print (t)                       #原始时间数据
+        #print (int(t))                  #秒级时间戳
+        #print (int(round(t * 1000)))    #毫秒级时间戳
+        #print (int(round(t * 1000000))) #微秒级时间戳
+        tt = str((int(t * 1000)))
+        ttt = str(int(round(t * 1000)))
 
-                #print (t)                       #原始时间数据
-                #print (int(t))                  #秒级时间戳
-                #print (int(round(t * 1000)))    #毫秒级时间戳
-                #print (int(round(t * 1000000))) #微秒级时间戳
-                tt = str((int(t * 1000)))
-                ttt = str(int(round(t * 1000)))
+        headers = {
+            'authority': 'gu.sina.cn',
+            'sec-ch-ua': '^\\^Google',
+            'sec-ch-ua-mobile': '?0',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
+            'accept': '*/*',
+            'sec-fetch-site': 'cross-site',
+            'sec-fetch-mode': 'no-cors',
+            'sec-fetch-dest': 'script',
+            'referer': 'https://finance.sina.com.cn/futures/quotes/'+symbol+'.shtml',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'cookie': 'ustat=__115.206.97.215_'+tt+'_0.65452100; genTime='+tt+'; vt=4; QUOTES-SINA-CN=',
+        }
 
-                headers = {
-                    'authority': 'gu.sina.cn',
-                    'sec-ch-ua': '^\\^Google',
-                    'sec-ch-ua-mobile': '?0',
-                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',
-                    'accept': '*/*',
-                    'sec-fetch-site': 'cross-site',
-                    'sec-fetch-mode': 'no-cors',
-                    'sec-fetch-dest': 'script',
-                    'referer': 'https://finance.sina.com.cn/futures/quotes/'+symbol+'.shtml',
-                    'accept-language': 'zh-CN,zh;q=0.9',
-                    'cookie': 'ustat=__115.206.97.215_'+tt+'_0.65452100; genTime='+tt+'; vt=4; QUOTES-SINA-CN=',
-                }
+        params = (
+            ('symbol', symbol),
+            ('type', str(minute)),
+        )
 
-                params = (
-                    ('symbol', symbol),
-                    ('type', str(minute)),
-                )
+        time.sleep(minute/100)
 
-                time.sleep(minute/100)
+        response = r.get('https://gu.sina.cn/ft/api/jsonp.php/var%5E%%5E20_'+symbol+'_'+str(minute)+'_'+ttt+'=/GlobalService.getMink', headers=headers, params=params ,timeout=1)
 
-                response = r.get('https://gu.sina.cn/ft/api/jsonp.php/var%5E%%5E20_'+symbol+'_'+str(minute)+'_'+ttt+'=/GlobalService.getMink', headers=headers, params=params ,timeout=1)
+        if response.cookies.get_dict(): #保持cookie有效 
+            s=r.session()
+            c = r.cookies.RequestsCookieJar()#定义一个cookie对象
+            c.set('cookie-name', 'cookie-value')#增加cookie的值
+            s.cookies.update(c)#更新s的cookie
+            s.get(url='https://gu.sina.cn/ft/api/jsonp.php/var%5E%%5E20_'+symbol+'_'+str(minute)+'_'+ttt+'=/GlobalService.getMink')
 
-                if response.cookies.get_dict(): #保持cookie有效 
-                    s=r.session()
-                    c = r.cookies.RequestsCookieJar()#定义一个cookie对象
-                    c.set('cookie-name', 'cookie-value')#增加cookie的值
-                    s.cookies.update(c)#更新s的cookie
-                    s.get(url='https://gu.sina.cn/ft/api/jsonp.php/var%5E%%5E20_'+symbol+'_'+str(minute)+'_'+ttt+'=/GlobalService.getMink')
+        s =response.text
+        s = s.strip('/*<script>location.href=\'//sina.com\';</script>*/\nvar^%^20_'+symbol+'_'+str(minute)+'_'+ttt+'=([')
+        s = s.strip('])')
+        values = eval(s)
+        df = pd.DataFrame(list(values))
+        df.columns=['date','open','high','low','close','volume','p']
+        df['volume'] = df['volume'].astype('float')
+        df.to_csv(symbol+str(minute)+'.csv',index=False)
+        df = pd.read_csv(symbol+str(minute)+'.csv')
+                
+        #保存数据 变换数据格式
+        res = pd.read_csv(symbol+'-'+str(minute)+'min.csv')
+        res = res.append([{'date':df['date'].values[-1],'open':df['open'].values[-1],'high':df['high'].values[-1],'low':df['low'].values[-1],'close':df['close'].values[-1],'volume':df['volume'].values[-1]}], ignore_index=True)
+        res['volume']=res['volume'].astype(np.float64)
+        res['close']=res['close'].astype(np.float64)
+        res['open']=res['open'].astype(np.float64)
+        res['high']=res['high'].astype(np.float64)
+        res['low']=res['low'].astype(np.float64)
+        res.to_csv(symbol+'-'+str(minute)+'min.csv')
+        res = self.getfulldata(res)
 
-                s =response.text
-                s = s.strip('/*<script>location.href=\'//sina.com\';</script>*/\nvar^%^20_'+symbol+'_'+str(minute)+'_'+ttt+'=([')
-                s = s.strip('])')
-                values = eval(s)
-                df = pd.DataFrame(list(values))
-                df.columns=['date','open','high','low','close','volume','p']
-                df['volume'] = df['volume'].astype('float')
-                df.to_csv(symbol+str(minute)+'.csv',index=False)
-                df = pd.read_csv(symbol+str(minute)+'.csv')
-                df['volume'].values[-1]
-                res = pd.read_csv(symbol+'-'+minute+'min.csv')
-                res = res.append([{'date':df['date'].values[-1],'open':df['open'].values[-1],'high':df['high'].values[-1],'low':df['low'].values[-1],'close':df['close'].values[-1],'volume':df['volume'].values[-1]}], ignore_index=True)
-                df['volume']=df['volume'].astype(np.float64)
-                df['close']=df['close'].astype(np.float64)
-                df['open']=df['open'].astype(np.float64)
-                df['high']=df['high'].astype(np.float64)
-                df['low']=df['low'].astype(np.float64)
-                res.to_csv(symbol+'-'+minute+'min.csv')
-                df = self.getfulldata(df)
+        time.sleep(minute/100)
 
-                time.sleep(minute/100)
-
-                return df
-                break
-            except:
-                time.sleep(5/100)
-                continue
+        return res
+                
     
 
     def getfulldata(self,df):
@@ -155,26 +151,18 @@ class NQ_Datainfo():
                 SendDingding.buy_sender("\n买入"+str(minute)+'分钟，'+symbol+"，符合条件，买入时间是："+str(df['date'][-1:].values[0])+"，买入值是："+str(df['close'][-1:].values),symbol)
             else:
                 print("\n"+str(df['date'][-1:].values[0])+"--->>>"+str(minute)+'分钟，--->>>'+symbol+"--->>>不满足条件")
+        else:
+            print("\n"+str(df['date'][-1:].values[0])+"--->>>"+str(minute)+'分钟，--->>>'+symbol+"--->>>不满足条件")
 
        
 
     #判断时间买卖
     def timepass(self):
-        if(datetime.now().weekday()+1!=7):
-
-                if((datetime.now().weekday()+1 >1 and datetime.now().weekday()+1<=6 and datetime.now() >= datetime.strptime(str(datetime.now().date())+' 00:00:00', '%Y-%m-%d %H:%M:%S') and datetime.now() <= datetime.strptime(str(datetime.now().date())+' 05:00:00', '%Y-%m-%d %H:%M:%S'))  or  (datetime.now().weekday()+1<=5 and datetime.now() >= datetime.strptime(str(datetime.now().date())+' 06:00:00', '%Y-%m-%d %H:%M:%S') and datetime.now() <= datetime.strptime(str(datetime.now().date())+' 23:59:59', '%Y-%m-%d %H:%M:%S'))):
-                    return True
-                else:
-                    return False
-        else:
-            return False
+        return True
 
     def time_hsi_quantian(self):
 
-        if((datetime.now().weekday()+1 >1 and datetime.now().weekday()+1<=6 and datetime.now() >= datetime.strptime(str(datetime.now().date())+' 00:00:00', '%Y-%m-%d %H:%M:%S') and datetime.now() <= datetime.strptime(str(datetime.now().date())+' 03:00:00', '%Y-%m-%d %H:%M:%S'))  or  (datetime.now().weekday()+1<=5 and datetime.now() >= datetime.strptime(str(datetime.now().date())+' 09:15:00', '%Y-%m-%d %H:%M:%S') and datetime.now() <= datetime.strptime(str(datetime.now().date())+' 12:00:00', '%Y-%m-%d %H:%M:%S'))  or (datetime.now().weekday()+1<=5 and datetime.now() >= datetime.strptime(str(datetime.now().date())+' 13:00:00', '%Y-%m-%d %H:%M:%S') and datetime.now() <= datetime.strptime(str(datetime.now().date())+' 16:30:00', '%Y-%m-%d %H:%M:%S')) or (datetime.now().weekday()+1<=5 and datetime.now() >= datetime.strptime(str(datetime.now().date())+' 17:15:00', '%Y-%m-%d %H:%M:%S') and datetime.now() <= datetime.strptime(str(datetime.now().date())+' 23:59:59', '%Y-%m-%d %H:%M:%S'))):
-            return True
-        else:
-            return False
+        return True
 
     def time_if_part_tian(self):
 
@@ -183,7 +171,28 @@ class NQ_Datainfo():
         else:
             return False
 
-    #NQ,CL,YM,HSI,ES,NK,CHA50CFD 5分钟
+
+    #NQ,YM,HSI 1分钟
+
+
+
+    def final_NQ_1M(self):
+        if(self.timepass()):
+            df = self.getdatainfo_minute(1,'NQ')
+            self.buyinfo(df,'NQ',1)
+
+    def final_YM_1M(self):
+        if(self.timepass()):
+            df = self.getdatainfo_minute(1,'YM')
+            self.buyinfo(df,'YM',1)
+
+    def final_HSI_1M(self):
+        if(self.time_hsi_quantian()):
+            df = self.getdatainfo_minute(1,'HSI')
+            self.buyinfo(df,'HSI',1)
+
+
+    #NQ,YM,HSI  5分钟
 
 
 
@@ -202,23 +211,10 @@ class NQ_Datainfo():
             df = self.getdatainfo_minute(5,'HSI')
             self.buyinfo(df,'HSI',5)
 
-    def final_ES_5M(self):
-        if(self.timepass()):
-            df = self.getdatainfo_minute(5,'ES')
-            self.buyinfo(df,'ES',5)
 
-    def final_NK_5M(self):
-        if(self.timepass()):
-            df = self.getdatainfo_minute(5,'NK')
-            self.buyinfo(df,'NK',5)
-
-    def final_CHA50CFD_5M(self):
-        if(self.time_if_part_tian()):
-            df = self.getdatainfo_minute(5,'CHA50CFD')
-            self.buyinfo(df,'CHA50CFD',5)
 
     
-    #NQ,CL,YM,HSI,ES,NK,CHA50CFD  15分钟
+    #NQ,YM,HSI  15分钟
     
     def final_NQ_15M(self):
         if(self.timepass()):
@@ -235,23 +231,50 @@ class NQ_Datainfo():
             df = self.getdatainfo_minute(15,'HSI')
             self.buyinfo(df,'HSI',15)
 
-    def final_ES_15M(self):
-        if(self.timepass()):
-            df = self.getdatainfo_minute(15,'ES')
-            self.buyinfo(df,'ES',15)
-
-    def final_NK_15M(self):
-        if(self.timepass()):
-            df = self.getdatainfo_minute(15,'NK')
-            self.buyinfo(df,'NK',15)
-
-    def final_CHA50CFD_15M(self):
-        if(self.time_if_part_tian()):
-            df = self.getdatainfo_minute(15,'CHA50CFD')
-            self.buyinfo(df,'CHA50CFD',15)
 
 
 class RuntimeScheduler:
+
+    #NQ,CL,YM,HSI,ES,NK,CHA50CFD
+
+    def get_final_NQ_1M_job(self):
+
+        
+        nqdata = NQ_Datainfo()
+        scheduler = BlockingScheduler()
+        scheduler.add_job((nqdata.final_NQ_1M), 'cron', minute='*/1')
+        print(scheduler.get_jobs())
+        try:
+            scheduler.start()
+        except KeyboardInterrupt:
+            scheduler.shutdown()
+        
+
+    def get_final_YM_1M_job(self):
+
+        
+        nqdata = NQ_Datainfo()
+        scheduler = BlockingScheduler()
+        scheduler.add_job((nqdata.final_YM_1M), 'cron', minute='*/1')
+        print(scheduler.get_jobs())
+        try:
+            scheduler.start()
+        except KeyboardInterrupt:
+            scheduler.shutdown()
+
+    def get_final_HSI_1M_job(self):
+
+        
+        nqdata = NQ_Datainfo()
+        scheduler = BlockingScheduler()
+        scheduler.add_job((nqdata.final_HSI_1M), 'cron', minute='*/1')
+        print(scheduler.get_jobs())
+        try:
+            scheduler.start()
+        except KeyboardInterrupt:
+            scheduler.shutdown()
+
+
 
 
     #NQ,CL,YM,HSI,ES,NK,CHA50CFD
@@ -293,41 +316,7 @@ class RuntimeScheduler:
         except KeyboardInterrupt:
             scheduler.shutdown()
 
-    def get_final_ES_5M_job(self):
 
-        
-        nqdata = NQ_Datainfo()
-        scheduler = BlockingScheduler()
-        scheduler.add_job((nqdata.final_ES_5M), 'cron', minute='*/5')
-        print(scheduler.get_jobs())
-        try:
-            scheduler.start()
-        except KeyboardInterrupt:
-            scheduler.shutdown()
-
-    def get_final_NK_5M_job(self):
-
-        
-        nqdata = NQ_Datainfo()
-        scheduler = BlockingScheduler()
-        scheduler.add_job((nqdata.final_NK_5M), 'cron', minute='*/5')
-        print(scheduler.get_jobs())
-        try:
-            scheduler.start()
-        except KeyboardInterrupt:
-            scheduler.shutdown()
-
-    def get_final_CHA50CFD_5M_job(self):
-
-        
-        nqdata = NQ_Datainfo()
-        scheduler = BlockingScheduler()
-        scheduler.add_job((nqdata.final_CHA50CFD_5M), 'cron', minute='*/5')
-        print(scheduler.get_jobs())
-        try:
-            scheduler.start()
-        except KeyboardInterrupt:
-            scheduler.shutdown()
 
 
 
@@ -369,43 +358,7 @@ class RuntimeScheduler:
         except KeyboardInterrupt:
             scheduler.shutdown()
 
-    def get_final_ES_15M_job(self):
 
-        
-        nqdata = NQ_Datainfo()
-        scheduler = BlockingScheduler()
-        scheduler.add_job((nqdata.final_ES_15M), 'cron', minute='*/15')
-        print(scheduler.get_jobs())
-        try:
-            scheduler.start()
-        except KeyboardInterrupt:
-            scheduler.shutdown()
-
-    def get_final_NK_15M_job(self):
-
-        
-        nqdata = NQ_Datainfo()
-        scheduler = BlockingScheduler()
-        scheduler.add_job((nqdata.final_NK_15M), 'cron', minute='*/15')
-        print(scheduler.get_jobs())
-        try:
-            scheduler.start()
-        except KeyboardInterrupt:
-            scheduler.shutdown()
-
-    def get_final_CHA50CFD_15M_job(self):
-
-        
-        nqdata = NQ_Datainfo()
-        scheduler = BlockingScheduler()
-        scheduler.add_job((nqdata.final_CHA50CFD_15M), 'cron', minute='*/15')
-        print(scheduler.get_jobs())
-        try:
-            scheduler.start()
-        except KeyboardInterrupt:
-            scheduler.shutdown()
-
-  
 
 
 #发钉钉的类先声明
@@ -513,55 +466,19 @@ if __name__ == '__main__':
     file = open(f'./datas/infodata.txt', 'w',encoding='utf-8').close()
     time.sleep(0.5)
 
-    res = pd.DataFrame(columns=('timestamps','close', 'KONGPAN1', 'KONGPAN2','X1', 'X2', 'Y1' ,'Y2', 'macd1','macd2'))
-    
-    pathlist = []
-    pathlist.append(f'./datas/okex/BTC-USD-SWAP/1minute/1minute.csv')
-    pathlist.append(f'./datas/okex/BTC-USD-SWAP/3minute/3minute.csv')
-    pathlist.append(f'./datas/okex/BTC-USD-SWAP/5minute/5minute.csv')
-    pathlist.append(f'./datas/okex/BTC-USD-SWAP/15minute/15minute.csv')
-    pathlist.append(f'./datas/okex/BTC-USD-SWAP/60minute/60minute.csv')
-    pathlist.append(f'./datas/okex/BTC-USD-SWAP/240minute/240minute.csv')
-
-    count = 0
-    for path in pathlist:
-        
-        minute = 0
-
-        if(count==0):
-            minute = 1
-        elif(count==1):
-            minute = 3
-        elif(count==2):
-            minute = 5
-        elif(count==3):
-            minute = 15
-        elif(count==4):
-            minute = 60
-        elif(count==5):
-            minute = 240
-
-        if(not os.path.exists(path)):
-
-            p = f'./datas/okex//'+str(minute)+'minute/'
-            try:
-                os.makedirs(p,exist_ok=True)
-            except:
-                pass
-            res.to_csv(path,index=0)
-        count+=1
 
     dingzhi =  RuntimeScheduler()
 
     #定义进程
-    p1 =multiprocessing.Process(target = dingzhi.get_final_YM_5M_job)
-    p2 =multiprocessing.Process(target = dingzhi.get_final_YM_15M_job)
-    p3 =multiprocessing.Process(target = dingzhi.get_final_NQ_5M_job)
-    p4 =multiprocessing.Process(target = dingzhi.get_final_NQ_15M_job)
-    p1 =multiprocessing.Process(target = dingzhi.get_final_ES_5M_job)
-    p2 =multiprocessing.Process(target = dingzhi.get_final_ES_15M_job)
-    p3 =multiprocessing.Process(target = dingzhi.get_final_NK_5M_job)
-    p4 =multiprocessing.Process(target = dingzhi.get_final_NK_15M_job)
+    p1 =multiprocessing.Process(target = dingzhi.get_final_YM_1M_job)
+    p2 =multiprocessing.Process(target = dingzhi.get_final_YM_5M_job)
+    p3 =multiprocessing.Process(target = dingzhi.get_final_YM_15M_job)
+    p4 =multiprocessing.Process(target = dingzhi.get_final_NQ_1M_job)
+    p5 =multiprocessing.Process(target = dingzhi.get_final_NQ_5M_job)
+    p6 =multiprocessing.Process(target = dingzhi.get_final_NQ_15M_job)
+    p7 =multiprocessing.Process(target = dingzhi.get_final_HSI_1M_job)
+    p8 =multiprocessing.Process(target = dingzhi.get_final_HSI_5M_job)
+    p9 =multiprocessing.Process(target = dingzhi.get_final_HSI_15M_job)
 
     #start
 
@@ -573,6 +490,7 @@ if __name__ == '__main__':
     p6.start()
     p7.start()
     p8.start()
+    p9.start()
 
     #join
 
@@ -584,4 +502,5 @@ if __name__ == '__main__':
     p6.join()
     p7.join()
     p8.join()
+    p9.join()
 
